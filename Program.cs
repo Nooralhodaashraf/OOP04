3498176cdd98a65ec3185114b70e17a4819c498d
Nooralhodaashraf <nooralhodaashraf@gmail.com>
2026-08-18 00:17:41 +0300

    Q1-a)

--- Program.cs
+++ Program.cs
@@ -4,7 +4,26 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Part 01 — Theoretical Questions
+
+            #region Q1  
+
+            #region a)  What is Abstraction in Object-Oriented Programming?
+            //a) Abstraction is a fundamental concept in Object-Oriented Programming (OOP) that focuses on simplifying complex systems by modeling classes based on the essential properties and behaviors of real-world entities. It allows developers to hide unnecessary implementation details and expose only the relevant features of an object, making it easier to understand and work with.
+            #endregion
+
+
+
+            #endregion
+
+
+
+
+
+
+            #endregion
+
+
         }
     }
 }
