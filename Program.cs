9527da1d10b0b4343c44fe60278950bc27cf0e3a
Nooralhodaashraf <nooralhodaashraf@gmail.com>
2026-08-18 00:19:58 +0300

    Q1-b)

--- Program.cs
+++ Program.cs
@@ -12,7 +12,9 @@
             //a) Abstraction is a fundamental concept in Object-Oriented Programming (OOP) that focuses on simplifying complex systems by modeling classes based on the essential properties and behaviors of real-world entities. It allows developers to hide unnecessary implementation details and expose only the relevant features of an object, making it easier to understand and work with.
             #endregion
 
-
+            #region b)  Why is abstraction considered one of the four pillars of OOP?
+            //abstraction is considered one of the 4 pillars of OOP because it hides unnecessary implementation details and exposes only the essential features of an object.
+            #endregion
 
             #endregion
 
